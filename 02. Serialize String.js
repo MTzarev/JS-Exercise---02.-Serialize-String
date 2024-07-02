05ceb08a336a8a11180e18834df7758fcd22548c
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-02 23:11:28 +0300

    Add files via upload

---
+++ 02. Serialize String.js
@@ -0,0 +1,20 @@
+function serializeString(input) {
+    let string = input[0];
+    let letters = [];
+    for (let i = 0; i < string.length; i++) {
+        if (!letters.includes(string[i])) {
+            letters.push(string[i]);
+        }
+    }
+    while (letters.length > 0) {
+        let symbol = letters.shift();
+        let indexes = [];
+        for (let i = 0; i < string.length; i++) {
+            if (symbol === string[i]) {
+                indexes.push(i);
+            }
+        }
+        console.log(`${symbol}:${indexes.join('/')}`);
+    }
+}
+serializeString(["MiroslavTzarev"]);
